eaeecdc4d93a2759948fc31a650fbd22b38b981b
Kaian Samuel <kaiansamuelster@gmail.com>
2024-01-01 20:07:11 -0300

    Termino do projeto

---
+++ src/MostraVoltas.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MostrarVoltas = (props) => {
+  return (
+    <p>
+        <span className='voltas'>{props.voltas}</span><br />
+        Voltas
+      </p>
+  )
+}
+export default MostrarVoltas
